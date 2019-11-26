862650c14b43ec44b169097e7a44f7c0cef79ed0
Mike Gallagher <mike.gallagh@gmail.com>
2019-11-26 12:13:10 -0600

    two other unnecessary spaces

--- src/templates/reset.js
+++ src/templates/reset.js
@@ -2,9 +2,7 @@ export default `<p>Hi {firstName},</br>
     </br>I was just doing a review of your CATS account and wanted to know if you were aware of the following features of CATS:</br></br>
     {list}</br>I'd encourage you to review our Knowledge Base articles I have linked with those items, but I would love to set up a time for a call to cover any questions that you may have about your CATS account as well. Please feel free to <a href="https://calendly.com/mike-cats/15min">use this link to book a time for that</a>.
         </br>
+        </br>Thanks, looking forward to hearing from you!
         </br>
-        Thanks, looking forward to hearing from you!
-        </br>
-        </br>
-        Mike
+        </br>Mike
         </p>`;
